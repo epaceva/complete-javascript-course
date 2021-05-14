4e42c3aa576debd834a4c52787b9a9dd46d40fc6
Evelina Patseva <evelina.patseva@musala.com>
2021-05-14 17:40:51 +0300

    Basic Skeleton

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,36 @@
+// LECTURE: Values and Variables
+/*
+let country = 'Bulgaria';
+let continent = 'Europe';
+let population = 6000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+// *******************************************
+
+// LECTURE: Data Types
+/*
+let isIsland = false;
+let language;
+let country = 'Bulgaria';
+let population = 6000000;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof population);
+*/
+// *******************************************
+
+// LECTURE: let, const and var
+/*
+language = 'bulgarian';
+const country = 'Bulgaria';
+const continent = 'Europe';
+const isIsland = false;
+*/
+// *******************************************
+
+// LECTURE: Basic Operators
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,73 @@
+// LECTURE: Values and Variables
+/*
+let js = 'amaizing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = "Matilda";
+console.log(firstName);
+*/
+// *******************************************
+
+// LECTURE: Data Types
+/*
+let javascriptIsFun = true;
+console.log(true);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonos');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+// *******************************************
+
+// LECTURE: let, const and var
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programer';
+job = 'teacher';
+
+lastName = 'Paceva';
+console.log(lastName);
+*/
+// *******************************************
+
+// LECTURE: Basic Operators
+
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+//2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Eva';
+const lastName = 'Paceva';
+console.log(firstName + + lastName);
+
+
+
+
+
