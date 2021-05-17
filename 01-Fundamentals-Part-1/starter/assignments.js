8779d83ce9a263b042d96eaaf2ad64412d07d97e
Evelina Patseva <evelina.patseva@musala.com>
2021-05-17 17:59:45 +0300

    Basic Operations and Strings

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -34,3 +34,27 @@ const isIsland = false;
 // *******************************************
 
 // LECTURE: Basic Operators
+/*
+const country = 'Bulgaria';
+const continent = 'Europe';
+const population = 7000000;
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+halfPopulation++;
+console.log(halfPopulation);
+const finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+console.log(population <= 33000000);
+const description = country + ' is in ' + continent + ', ans its ' + population + ' people speak bulgarian';
+console.log(description);
+*/
+// *******************************************
+
+//  Strings and Template Literals
+/*
+const country = 'Bulgaria';
+const continent = 'Europe';
+const population = 7000000;
+const description = `${country} is in ${continent}, and its ${population} people speak bulgarian!`;
+console.log(description);
+*/
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,7 +54,7 @@ console.log(lastName);
 // *******************************************
 
 // LECTURE: Basic Operators
-
+/*
 const now = 2037
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -65,7 +65,63 @@ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
 const firstName = 'Eva';
 const lastName = 'Paceva';
-console.log(firstName + + lastName);
+console.log(firstName + ' ' + lastName);
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25;
+x *= 4 // x = x * 4 = 100;
+x++; // x = x + 1 =  101;
+x--; // x = x - 1 = 100;
+console.log(x);
+
+//Comparison operators
+// >, <, >=, <=
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2020)
+*/
+// *******************************************
+
+// Operator Precedence
+/*
+const now = 2037
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+// *******************************************
+
+//  Strings and Template Literals
+/*
+const firstName = 'Jonas';
+const job = 'teached';
+const birthYear = '1991';
+const year = 2037;
+
+const jonas = "I`m " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log('String with \n\
+multiple \n\
+lines' );
+console.log(`String with
+multiple
+lines`);
+*/
 
 
 
