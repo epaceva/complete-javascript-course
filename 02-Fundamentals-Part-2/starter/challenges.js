f6befc83d09e6a0c796719eb85950253b3a1dd8e
Evelina Patseva <evelina.patseva@musala.com>
2021-05-31 16:15:36 +0300

    Challenge1

---
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -0,0 +1,27 @@
+'use strict';
+//Challenge #1
+/*
+const calcAverage = (firstScore, secondScore, thirdScore) => (firstScore + secondScore + thirdScore) / 3;
+//poitn1
+let avgDolphins = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`;
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`;
+    } else {
+        return `No winner`
+    }
+}
+
+console.log(avgDolphins, avgKoalas);
+console.log(checkWinner(avgDolphins, avgKoalas));
+//point2
+avgDolphins = calcAverage(85, 54, 41);
+avgKoalas = calcAverage(23, 34, 27);
+console.log(avgDolphins, avgKoalas);
+console.log(checkWinner(avgDolphins, avgKoalas));
+*/
+
