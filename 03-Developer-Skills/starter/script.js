463ab9be84ab41a40b0f7ed54b5d7b56d0d0537e
Eva <paceva.eva@gmail.com>
2021-06-13 14:22:43 +0300

    Min, Max and tempriture amplitude

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,45 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+/*
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
+//Problem #1
+const calcTempAmplitude = function(temp) {
+    let max = temp[0];
+    let min = temp[0];
+
+    for(let i = 0; i < temp.length; i++){
+        const curTemp = temp[i]
+        if(typeof curTemp !== 'number') continue;
+
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    }
+    console.log(min, max);
+    return max - min;
+};
+ const amplitude = calcTempAmplitude(temperatures);
+ console.log(amplitude);
+ 
+//Problem #2
+
+const calcTempAmplitudeNew = function(t1, t2) {
+const temp = t1.concat(t2);
+console.log(temp);
+
+    let max = temp[0];
+    let min = temp[0];
+
+    for(let i = 0; i < temp.length; i++){
+        const curTemp = temp[i]
+        if(typeof curTemp !== 'number') continue;
+
+        if(curTemp > max) max = curTemp;
+        if(curTemp < min) min = curTemp;
+    }
+    console.log(min, max);
+    return max - min;
+};
+ const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+ console.log(amplitudeNew);
+ */
