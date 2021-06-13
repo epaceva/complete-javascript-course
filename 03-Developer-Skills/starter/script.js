5772be6065ca830c1a5e60a43cf15402474b08dd
Eva <paceva.eva@gmail.com>
2021-06-13 15:36:21 +0300

    Challenge#1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -92,3 +92,21 @@ console.log(temp);
      // A) INDENTIFIED
      console.log(amplitudeBug);
      */
+    //****************************
+    //Challenge#1
+    /*
+    const arr1 = [17, 21, 23];
+    const arr2 = [12,5, -5, 0, 4];
+
+    const printForecast = function(arr){
+        let str = '';
+        for(let i = 0; i < arr.length; i++){
+            str = str + `${arr[i]}ºC in ${i + 1}days ... `;
+        }
+        console.log('...' + str);
+    }
+    
+    printForecast(arr1);
+    printForecast(arr2);
+*/
+    
