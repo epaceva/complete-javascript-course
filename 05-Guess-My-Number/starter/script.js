a8b5ddfe8523a7bb3667377ac487062422b582f6
Eva <paceva.eva@gmail.com>
2021-06-20 19:13:23 +0300

    Change

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,7 +13,6 @@ console.log(document.querySelector('.guess').value);
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
@@ -26,6 +25,8 @@ document.querySelector('.check').addEventListener('click', function(){
     //When player wins
     else if( guess === secretNumber){
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+
         document.querySelector('body').style.backgroundColor = '#60b347'
 
         document.querySelector('.number').style.width = '30rem';
